f51ea08a87df6a2cb85ade7a34d26e45755e5224
MuriloM <muriloommaia@gmail.com>
2022-05-12 11:07:32 -0300

    fix: aplica --fix do eslint

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,20 +1,31 @@
 import React from 'react';
 
 class Pokemon extends React.Component {
-    render() {
-        const { name, type, averageWeight, image } = this.props.pokemon;
+  render() {
+    const { name, type, averageWeight, image } = this.props.pokemon;
 
-        return (
-            <div className="pokemon">
-                <div>
-                    <p> {name} </p>
-                    <p> {type} </p>
-                    <p> {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
-                </div>
-                <img src={image} alt={`${name} sprite`} />
-            </div>
-        );
-    }
+    return (
+      <div className="pokemon">
+        <div>
+          <p>
+            {' '}
+            {name}
+            {' '}
+          </p>
+          <p>
+            {' '}
+            {type}
+            {' '}
+          </p>
+          <p>
+            {' '}
+            {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}
+          </p>
+        </div>
+        <img src={ image } alt={ `${name} sprite` } />
+      </div>
+    );
+  }
 }
 
 export default Pokemon;
